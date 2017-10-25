78743e94e888c48683bf98139c35d91851a6c2ba
Unknown <jack.klodzinski@dot.state.fl.us>
2017-10-25 09:02:06 -0400

    Analysis Update
    Corrected connections between the OD matrix analysis and the ELToD results files for summary comparisons.  Also updated the speed in the model for segment 3 and reviewed questions from the project meeting.

--- Scripts/02_CSV to MAT/CSV_to_MAT_Y1.s
+++ Scripts/02_CSV to MAT/CSV_to_MAT_Y1.s
@@ -1,6 +1,6 @@
 
 RUN PGM=MATRIX ;converting a csv to cube matrix
-FILEI MATI[1] = "2025_AM.csv",
+FILEI MATI[1] = "AM.csv",
   PATTERN=IJ:V, FIELDS=#1,0,2-33
 FILEO MATO[1] = "2025 AM.MAT",
   MO=1-3 NAME=SOV,HOV,TRK
@@ -12,7 +12,7 @@ MW[1]=MI.1.1
 ENDRUN
 
 RUN PGM=MATRIX ;converting a csv to cube matrix
-FILEI MATI[1] = "2025_PM.csv",
+FILEI MATI[1] = "PM.csv",
   PATTERN=IJ:V, FIELDS=#1,0,2-33
 FILEO MATO[1] = "2025 PM.MAT",
   MO=1-3 NAME=SOV,HOV,TRK
@@ -24,7 +24,7 @@ MW[1]=MI.1.1
 ENDRUN
 
 RUN PGM=MATRIX ;converting a csv to cube matrix
-FILEI MATI[1] = "2025_OP.csv",
+FILEI MATI[1] = "OP.csv",
   PATTERN=IJ:V, FIELDS=#1,0,2-33
 FILEO MATO[1] = "2025 OP.MAT",
   MO=1-3 NAME=SOV,HOV,TRK
